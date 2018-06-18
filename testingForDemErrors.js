6ef61154406db7da033d9fb453465dd26910e222
snarkyllama <malak.pikachu.lover@gmail.com>
2018-06-17 20:10:52 -0400

    hm...

--- testingForDemErrors.js
+++
@@ -1,9 +0,0 @@
-const { HentaiClient } = require("./lib/index");
-
-const OwO = new HentaiClient({
-    useragent: "Hentai.JS/Development/1.3.4"
-});
-
-OwO.hentai().then((r) => {
-    console.log(r);
-});
